7a9cdb2e260762730407493e01a8f88436001871
katarinasokcevic <katarina.sokcevic.vk@gmail.com>
2022-11-13 17:16:09 +0100

    Pozdrav od mene

---
+++ Project1/Project1/Source.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	printf("ka si\n\n");
+	printf("kako si\n\n");
+	return 0;
+}
---
+++ Project1/Project1/vjezba.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("Zovem se Katarina Sokcevic");
+
+	printf("dobro je\n");
+
+	return 0;
+}
